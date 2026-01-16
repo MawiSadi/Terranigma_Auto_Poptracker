28465b0678fbcfe828e97533654856ea4a50a239
Sacko <126669591+MawiSadi@users.noreply.github.com>
2026-01-16 15:24:00 +0100

    added switch for disabling auto map tab

--- scripts/autotracking/map_tabs.lua
+++ scripts/autotracking/map_tabs.lua
@@ -2,6 +2,11 @@ ScriptHost:LoadScript("scripts/autotracking/mappings/tab_mapping.lua")
 
 local _lastTab = nil
 
+local function is_auto_map_switch_enabled()
+    local enabled =  Tracker:ProviderCountForCode("opt_auto_map_switch_on") == 1
+    return enabled ~= nil and enabled
+end
+
 local function activateTabPath(path)
     for seg in string.gmatch(path, "[^/]+") do
         Tracker:UiHint("ActivateTab", seg)
@@ -24,31 +29,31 @@ function terranigma_tab_for_mapid(mapId)
 end
 
 function updateCurrentMap(_)
-    local mapId = AutoTracker:ReadU16(CURRENT_MAP_ID_ADDR)
-    if mapId == nil then return end
+    if is_auto_map_switch_enabled() then
+        local mapId = AutoTracker:ReadU16(CURRENT_MAP_ID_ADDR)
+        if mapId == nil then return end
 
-    local tabPath = tab_for_mapid(mapId)
-    dbg("mapId=%04X -> tab=%s", mapId, tabPath)
+        local tabPath = tab_for_mapid(mapId)
+        dbg("mapId=%04X -> tab=%s", mapId, tabPath)
 
-    if tabPath ~= _lastTab then
-        activateTabPath(tabPath)
-        _lastTab = tabPath
-    end
+        if tabPath ~= _lastTab then
+            activateTabPath(tabPath)
+            _lastTab = tabPath
+        end
 
-    if mapId == 0x02CD and not TERRA_AT.litz_capture_armed then
-        TERRA_AT.litz_capture_armed = true
+        if mapId == 0x02CD and not TERRA_AT.litz_capture_armed then
+            TERRA_AT.litz_capture_armed = true
 
-        -- misc watcher baseline neu setzen (wichtig!)
-        if terranigma_reset_chest_snapshot_misc then terranigma_reset_chest_snapshot_misc() end
+            -- misc watcher baseline neu setzen (wichtig!)
+            if terranigma_reset_chest_snapshot_misc then terranigma_reset_chest_snapshot_misc() end
 
-        -- seen bucket für misc leeren (damit Rising-Edges wieder kommen)
-        TERRA_AT.seen_flags = TERRA_AT.seen_flags or {}
-        TERRA_AT.seen_flags["chest_misc"] = {}
+            -- seen bucket für misc leeren (damit Rising-Edges wieder kommen)
+            TERRA_AT.seen_flags = TERRA_AT.seen_flags or {}
+            TERRA_AT.seen_flags["chest_misc"] = {}
 
-        -- jetzt die nächsten X misc-rising logs zulassen
-        MISC_CAPTURE_MAP = 0x02CD
-        MISC_CAPTURE_LEFT = 30
-
-        dbg("LITZ CAPTURE ARMED: open the chest now")
+            -- jetzt die nächsten X misc-rising logs zulassen
+            MISC_CAPTURE_MAP = 0x02CD
+            MISC_CAPTURE_LEFT = 30
+        end
     end
 end
