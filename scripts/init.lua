e2d18e595a9dc999fa9ea6b2e88a09ab45367c08
Sacko <126669591+MawiSadi@users.noreply.github.com>
2026-03-03 00:38:21 +0100

    Added new layout and removed obsolete stuff

--- scripts/init.lua
+++ scripts/init.lua
@@ -2,8 +2,9 @@ Tracker:AddMaps("maps/maps.json")
 
 Tracker:AddLayouts("layouts/broadcast.json")
 Tracker:AddLayouts("layouts/item_grid.json")
+Tracker:AddLayouts("layouts/grouped_items.json")
+Tracker:AddLayouts("layouts/settings_popup.json")
 Tracker:AddLayouts("layouts/map_tracker.json")
-Tracker:AddLayouts("layouts/full.json")
 
 ScriptHost:LoadScript("scripts/loadlocations.lua")
 ScriptHost:LoadScript("scripts/loaditems.lua")
